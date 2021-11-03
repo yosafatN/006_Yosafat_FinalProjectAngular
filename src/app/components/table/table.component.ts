a5dca3a7e17010da2c3b98d1afd200e95db0dcb3
yosafatN <yosafatnababan77@gmail.com>
2021-11-04 01:39:26 +0700

    done error

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,10 +1,20 @@
-import { Component, Output, EventEmitter, Input, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, Output, EventEmitter, Input, AfterViewInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { MatSort } from '@angular/material/sort';
+import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Observable, Subscription } from 'rxjs';
 import { PaymentModel } from '../models/user';
+
+const dataInit: PaymentModel[] = [
+  {
+    paymentDetailId: 0,
+    cardOwnerName: 'Data Dummy',
+    cardNumber: '1234567890654321',
+    expirationDate: '11/24',
+    securityCode: '1234'
+  }
+]
 
 @Component({
   selector: 'app-table',
@@ -22,37 +32,29 @@ export class TableComponent implements AfterViewInit {
 
   private loginSubscription: Subscription = new Subscription
   @Input() updateData: Observable<PaymentModel[]> = new Observable<PaymentModel[]>()
-  
-  dataInit: PaymentModel[] = [
-    {
-      paymentDetailId: 0,
-      cardOwnerName: 'Data Dummy',
-      cardNumber: '1234567890654321',
-      expirationDate: '11/24',
-      securityCode: '1234'
-    }
-  ]
 
-  dataTable: MatTableDataSource<PaymentModel> = new MatTableDataSource(this.dataInit)
+  dataTable: MatTableDataSource<PaymentModel>
 
-  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
-  @ViewChild(MatSort) sort: MatSort | undefined;
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
 
   displayedColumns: string[] = ['cardOwnerName','cardNumber','expirationDate','actions']
   clickedRows = new Set<PaymentModel>();
 
-  constructor(private snackBar: MatSnackBar) { }
+  constructor(private snackBar: MatSnackBar) {
+    this.dataTable = new MatTableDataSource(dataInit)
+  }
 
   ngAfterViewInit() {
-    this.dataTable.paginator = this.paginator!
-    this.dataTable.sort = this.sort!
+    this.dataTable.paginator = this.paginator
+    this.dataTable.sort = this.sort
 
     this.eventSubscription = this.events.subscribe(() => {
       this.clickedRows.clear()
     })
 
     this.loginSubscription = this.updateData.subscribe((res) => {
-      this.dataTable = new MatTableDataSource(res)
+      this.dataTable.data = res
     })
   }
 
